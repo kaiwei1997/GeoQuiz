fdaebc79dfa464ebc611fa65c2b1dde0ae777047
Josh Tan <kaiweitan128@gmail.com>
2018-09-28 11:52:45 +0800

    NA

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -50,10 +50,10 @@ public class CheatActivity extends AppCompatActivity {
         }else{
             mIsCheat = false;
             setAnswerShownResult(false);
-        }
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
+    }
         mAnswerTextView  = (TextView)findViewById(R.id.answer_text_view);
         mShowAnswerButton = (Button)findViewById(R.id.show_answer_button);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
