b84ad2c8e51146dbe2175185e6622d8529c2c93b
Josh Tan <kaiweitan128@gmail.com>
2018-10-12 15:07:47 +0800

    CSC2074

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/StartingActivity.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.geoquiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartingActivity extends AppCompatActivity {
+
+    private Button startQuiz;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting);
+
+        startQuiz = (Button)findViewById(R.id.start_quiz_button);
+        startQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                start_quiz();
+            }
+        });
+    }
+
+    private void start_quiz(){
+        Intent intent = new Intent(StartingActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
